e908da9e0f514747dbd5260622516eceb95e9677
Dor <dor.shacham@gmail.com>
2022-10-16 11:24:28 +0300

    FINAL

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 int main()
 {
     system("clear");
-    torus source(L/3,L/12);
+    torus source(L*0.35,L*0.12);
 
     double deg = 1;
     unsigned int sleep_time = 5; //microsecond
     int counter=0;
     while(true)
     {
-        torus torus1 = source.rotateY(deg*(counter++)).rotateZ(deg*(counter++));
+        torus torus1 = source.rotateY(2*deg*(counter++)).rotateZ(deg*(counter++));
         torus1.screen_projection().continues().show();
         usleep(sleep_time);
     }
